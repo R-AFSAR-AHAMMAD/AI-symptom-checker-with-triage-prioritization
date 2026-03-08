ada4119fbb7fe504f5c16bb718da5cd63f141cc8
R-AFSAR-AHAMMAD <afsarahmed592@gmail.com>
2026-03-08 12:19:37 +0530

    feat: connected MongoDB
    - installed mongoose
    - created config/db.js for database connection
    - added MONGO_URI to .env
    - connected DB in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
